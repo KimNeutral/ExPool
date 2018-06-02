7268030783b9d9394442d73b526a2a3d7d2de804
대용 김 <01rlaeodyd@naver.com>
2018-06-03 01:25:22 +0900

    페이지 관리하는 스택 클래스 추가

--- ExPool/Home.xaml.cs
+++ ExPool/Home.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExPool.Pages;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,43 @@ using System.Windows.Shapes;
 
 namespace ExPool
 {
+    public class PageStack : INotifyPropertyChanged
+    {
+        private Stack<Page> stack = new Stack<Page>();
+
+        public Page CurrentPage
+        {
+            get
+            {
+                if (stack.Count != 0)
+                    return stack.Peek();
+                else
+                    return null;
+            }
+        }
+
+        public PageStack()
+        {
+            Add(new HomePage());
+        }
+
+        public void Add(Page page)
+        {
+            stack.Push(page);
+            NotifyPropertyChanged(nameof(CurrentPage));
+        }
+
+        public void Pop()
+        {
+            if (stack.Count == 1) return;
+            stack.Pop();
+            NotifyPropertyChanged(nameof(CurrentPage));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName) => PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+    }
+
     /// <summary>
     /// Home.xaml에 대한 상호 작용 논리
     /// </summary>
@@ -22,6 +61,7 @@ namespace ExPool
         public Home()
         {
             InitializeComponent();
+            this.DataContext = new PageStack();
         }
     }
 }
