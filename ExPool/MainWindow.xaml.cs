70a4ef7ab864b18840e98c062c884abac98cdde7
kanhel P. K. M <kanhelpk@gmail.com>
2018-06-02 21:33:30 +0900

    변경된 내용의 주석 삭제

--- ExPool/MainWindow.xaml.cs
+++ ExPool/MainWindow.xaml.cs
@@ -24,29 +24,7 @@ namespace ExPool
     /// </summary>
     public partial class MainWindow : Window
     {
-        /*private string Request_Json()
-        {
-            string result = null;
-            string url = "hhttp://expool-env.pns7m3byyt.us-east-2.elasticbeanstalk.com/auth";
-            //Console.WriteLine("url : " + url);
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                Stream stream = response.GetResponseStream();
-                StreamReader reader = new StreamReader(stream);
-                result = reader.ReadToEnd();
-                stream.Close();
-                response.Close();
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-
-            return result;
-        }*/
+        
         public MainWindow()
         {
             InitializeComponent();
