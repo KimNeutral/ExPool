f2db4558313a2287f0888dcde6f106266747817e
대용 김 <01rlaeodyd@naver.com>
2018-06-02 22:33:05 +0900

    네트워크 통신 모듈 추가

---
+++ ExPool/Network/TResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExPool.Network
+{
+    public class TResponse<T>
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+
+    public sealed class Nothing
+    {
+        public static Nothing AtAll => null;
+    }
+}
