ada25135f4eab216fced370c1058ad14e2fff252
홍준의 <akdlektm18@gmail.com>
2018-06-02 19:06:45 +0900

    일단 대충 디자인 짠거

--- ExPool/MainWindow.xaml.cs
+++ ExPool/MainWindow.xaml.cs
@@ -12,17 +12,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
 
 namespace ExPool
 {
+
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*private string Request_Json()
+        {
+            string result = null;
+            string url = "hhttp://expool-env.pns7m3byyt.us-east-2.elasticbeanstalk.com/auth";
+            //Console.WriteLine("url : " + url);
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return result;
+        }*/
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Request_Json();
         }
     }
 }
